fe5ffdf5bb125588501a2f8b8961e7e756abb487
mithunraj <mithun.raj159@gmail.com>
2020-09-06 10:02:00 +0530

    Initial Commit

---
+++ ToolBarSharingAppMacOS/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ToolBarSharingAppMacOS
+//
+//  Created by Mithun Raj on 06/09/20.
+//  Copyright © 2020 Mithun Raj. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ToolBarSharingAppMacOS/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ToolBarSharingAppMacOS
+//
+//  Created by Mithun Raj on 06/09/20.
+//  Copyright © 2020 Mithun Raj. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
